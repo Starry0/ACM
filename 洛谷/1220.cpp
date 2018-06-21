6febd1e1ecb6e805bf0d22ecd010059d8c5d3b7a
李港 <1173298382@qq.com>
2018-06-21 17:07:05 +0800

    2018-6-21

--- 洛谷/1220.cpp
+++ 洛谷/1220.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 #define INF 0x3f3f3f3f
 using namespace std;
 const int N = 55;
@@ -21,4 +21,54 @@ int main() {
     }
     printf("%d\n",min(dp[1][n][0],dp[1][n][1]));
     return 0;
+}
+*/
+
+
+#include<iostream>
+using namespace std;
+int a[55];  //位置
+int b[55];  //功率
+bool c[55];
+int n,d;
+int ans=99999999;
+int w;//此秒总功率
+void hh(int nw,int now,int ans1,int nn)
+{	if(ans1>ans) return ;
+	if(nn==0){
+		ans=min(ans,ans1);
+		return;
+	}
+	int i;
+	i=now-1;
+	while(c[i]==false&&i>0) i--;
+	if(c[i]&&i>0){
+			c[i]=false;
+			hh(nw-b[i],i,nw*(a[now]-a[i])+ans1,nn-1);
+			c[i]=true;
+		    }
+	i=now+1;
+	while(c[i]==false&&i<=n)i++;
+	if(c[i]&&i<=n){
+			c[i]=false;
+			hh(nw-b[i],i,nw*(a[i]-a[now])+ans1,nn-1);
+			c[i]=true;
+		}
+}
+
+int main(){
+	cin>>n>>d;
+	int x,y;
+	for(int i=1;i<=n;i++){
+		cin>>x>>y;
+		a[i]=x;
+		b[i]=y;
+		c[i]=true;
+		w+=y;
+	}
+	c[d]=false;
+	w=w-b[d];
+	hh(w,d,0,n-1);
+	cout<<ans;
+	return 0;
 }
